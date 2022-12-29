d628bc01b0957bfe4a398141ead53eae23584ece
Mario Hawat <hawat.mario@gmail.com>
2022-12-29 18:08:44 +0100

    Update blender_dashboard.py

--- blender_dashboard.py
+++ blender_dashboard.py
@@ -60,7 +60,7 @@ df = pd.read_csv('tweet_ranking.csv')
 
 try:
     n = st.slider('Number of tweets to show', 10, 100)
-    st.dataframe(df[['URL', 'engagement_score', 'like_count', 'reply_count', 'retweet_count', 'quote_count']],
+    st.dataframe(df[['URL', 'engagement_score', 'like_count', 'reply_count', 'retweet_count', 'quote_count']][:n],
                 width=700, height=300)
     st.markdown(f'Showing top {n} tweets')
 except:
