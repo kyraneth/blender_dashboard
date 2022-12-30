0ed907585549a2ebcfd8d8e3a3dcd2c1524b715a
Mario Hawat <hawat.mario@gmail.com>
2022-12-30 02:29:30 +0100

    Update blender_dashboard.py

--- blender_dashboard.py
+++ blender_dashboard.py
@@ -98,7 +98,7 @@ with tab1:
           st.markdown(":red[Data might be out of date, generate data]")
           st.write("Data might be out of date, generate data")
         else:
-          st.write("Data less than 1 hour old")
+          st.write(":green[Data less than 1 hour old]")
 
   df = pd.read_csv('tweet_ranking.csv')
 
