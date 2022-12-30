ba997c9cce25ce2d68b93d5afdaf63b0bb64ed23
Mario Hawat <hawat.mario@gmail.com>
2022-12-30 01:06:36 +0100

    Update blender_dashboard.py

--- blender_dashboard.py
+++ blender_dashboard.py
@@ -24,6 +24,7 @@ def search_and_rank_tweets():
   # Search for tweets
   tweets = tweepy.Paginator(client.search_recent_tweets, query=query,
                                        tweet_fields=['public_metrics','author_id','text','created_at'],
+                                       user_fields=['username'],
                                        max_results=100).flatten(limit=100000)
 
   tweet_data = []
